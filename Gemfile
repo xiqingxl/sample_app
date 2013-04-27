24a311a5f1d1bd49941c911b06802f0490161ff2
xiqingxl <617524695@qq.com>
2013-04-27 12:52:21 +0800

    Make a basic user model including secure passwords

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
 end
-
+gem 'bcrypt-ruby', '3.0.1'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new(name: "Example User",
+                     email: "user@example.com,
+                     password: "foobar",
+                     password_confirmation: "foobar")
   end
 end
