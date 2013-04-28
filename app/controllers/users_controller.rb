96da26bac3c0f2d1da1f491b72388c21ef3c76f3
xiqingxl <617524695@qq.com>
2013-04-28 19:33:15 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all   
+  end
+  
   def new
-    @user = User.new(name: "Example User",
-                     email: "user@example.com,
-                     password: "foobar",
-                     password_confirmation: "foobar")
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "welcome to the Sample App"
+      redirect_to @user
+    else
+     render 'new'
+    end
+  end   
+   
+  def show
+    @user = User.find(params[:id])
   end
 end
